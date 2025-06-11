2826c4633220e12031806214bd35d81162b33ac4
Mohan <mohan@stackbox.xyz>
2025-06-11 13:56:00 +0530

    fix(catpuccin): :bug: fix theme and curson issue over ssh

--- init.lua
+++ init.lua
@@ -6,7 +6,9 @@ if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
     lazypath })
 end
+
 vim.opt.rtp:prepend(lazypath)
+vim.opt.clipboard = "unnamedplus"
 
 -- validate that lazy is available
 if not pcall(require, "lazy") then
--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -13,8 +13,4 @@ return {
     -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
     { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
   },
-
-  require("venv-selector").setup {
-    poetry_path = "/home/cado/.cache/pypoetry/virtualenvs",
-  },
 }
