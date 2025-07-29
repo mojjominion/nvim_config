9ba3ed29f0efe7c5bd7ec7ca32f4859b8aa547ac
Mohan <mohan.lal@samsung.com>
2025-07-29 10:20:02 +0530

    fix(erlang): :bug: Fix erlang debug logging

--- lua/plugins/debugprint.lua
+++ lua/plugins/debugprint.lua
@@ -6,15 +6,23 @@ return {
     "nvim-treesitter/nvim-treesitter",
   },
   opts = { ... },
-  -- config = function(...)
-  --   local javascript = {
-  --     left = 'console.warn("',
-  --     right = '")',
-  --     mid_var = '", ',
-  --     right_var = ")",
-  --   }
-  --   require("debugprint").setup { filetypes = { javascript } }
-  -- end,
+  config = function(...)
+    -- local javascript = {
+    --   left = 'console.warn("',
+    --   right = '")',
+    --   mid_var = '", ',
+    --   right_var = ")",
+    -- }
+    -- require("debugprint").setup { filetypes = { javascript } }
+    local erlang = {
+      left = 'io:format("',
+      -- left_var = 'print "', -- `left_var` is optional, for 'variable' lines only; `left` will be used if it's not present
+      right = '"),',
+      mid_var = '", ',
+      right_var = "),",
+    }
+    require("debugprint").setup { filetypes = { ["erlang"] = erlang, ... } }
+  end,
   -- Remove the following line to use development versions,
   -- not just the formal releases
   version = "*",
