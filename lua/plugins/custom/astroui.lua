18a5a1d417e3f2be7d4ba373e25417eb7ae83d4f
Mohan <mohan.lal@samsung.com>
2025-08-01 10:15:21 +0530

    feat(tokyonight): :sparkles: add tokyonight

--- lua/plugins/custom/astroui.lua
+++ lua/plugins/custom/astroui.lua
@@ -12,48 +12,7 @@ return {
   -- further customize the options set by the community
   opts = {
     colorscheme = "catppuccin-macchiato",
-    style = "macchiato", -- latte, frappe, macchiato, mocha
-    -- background = { -- :h background
-    --   light = "latte",
-    --   dark = "frappe",
-    -- },
-    -- -- transparent_background = false, -- disables setting the background color.
-    -- show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-    -- term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-    -- dim_inactive = {
-    --   enabled = false, -- dims the background color of inactive window
-    --   shade = "dark",
-    --   percentage = 0.15, -- percentage of the shade to apply to the inactive window
-    -- },
-    -- no_italic = false, -- Force no italic
-    -- no_bold = false, -- Force no bold
-    -- no_underline = false, -- Force no underline
-    -- styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-    --   comments = { "italic" }, -- Change the style of comments
-    --   conditionals = { "italic" },
-    --   loops = {},
-    --   functions = {},
-    --   keywords = {},
-    --   strings = {},
-    --   variables = {},
-    --   numbers = {},
-    --   booleans = {},
-    --   properties = {},
-    --   types = {},
-    --   operators = {},
-    -- },
-    -- custom_highlights = {},
-    -- color_overrides = {
-    --   all = {
-    --     text = "#ffffff",
-    --   },
-    --   mocha = {
-    --     base = "#1e1e2e",
-    --   },
-    --   frappe = {},
-    --   macchiato = {},
-    --   latte = {},
-    -- },
+    style = "macchiato",
     integrations = {
       sandwich = false,
       noice = true,
